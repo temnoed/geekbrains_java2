1192086494ade4d9a34638213ad5d86ab11c78db
temnoed <dc9257351633@gmail.com>
2017-02-22 06:34:39 +0300

    Условие дз.

--- src/ru/geekbrains/java2/dz/dz3/MainClass.java
+++ src/ru/geekbrains/java2/dz/dz3/MainClass.java
@@ -1,11 +1,32 @@
+/*
+*	1) Ввод в консоль для каждого пассажира: имя, номер документа, номер рейса.
+*
+*	2)	Нужно реализовать рассадку пассажирова по рейсам. Каждого пассажира сажаем на свой
+*	рейс. Самолет может иметь или не иметь пассажиров.
+*
+*	3) По окончанию ввода пассажиров в консоль выводится список всех рейсов.
+*	Для каждого рейса указывается количество пассажиров на нем.
+*	Рейсы должны быть упорядочены по номеру.
+*
+*	4) и вывод отправляющихся на каждом рейсе пассажиров (имя и номер документа).
+*	пассажиры упорядочены по имени.
+*
+*	Подсказки: класс Passenger, интерфейсы List (для хранения пассажировов в рейсе)
+*	и Map (для сопоставления имени рейса и пассажиров в рейсе).
+*	List<Passenger> passengers = new ArrayList<Passenger>();
+*	Scanner scanner = new Scanner(System.in);
+*	int flightNumber = scanner.nextInt();
+*	Map<Integer,List<Passenger>> passengers = new TreeMap<Integer,List<Passenger>>
+*	Collection<Passenger> = new ArrayList<Passenger>();
+*	for (Passenger passenger : passengers) { }
+*
+ */
+
 package ru.geekbrains.java2.dz.dz3;
 
 public class MainClass {
 
 	public static void main(String[] args) {
-
-
-		
 
 
 	}
